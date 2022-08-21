2fe2ec0d2829645ef193c26ae2d8342e8d1d550d
tarak899 <tarakram8999@gmail.com>
2022-08-21 23:57:25 +0530

    first commit

---
+++ database.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "den1.mysql1.gear.host";
+$username = "schoolnew";
+$password = "Ms5x8mZS__11";
+$dbname = "schoolnew";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
